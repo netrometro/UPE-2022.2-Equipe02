48546b9c84f0c060f49f2a0f77ae144d9f278a65
David Rocha <manoeudavi3@gmail.com>
2023-03-02 21:53:06 -0300

    creating the project

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config()
+const app = express();
+
+//liberar qualquer acesso (domínio/IP)
+app.use(cors())
+app.use(express.json())
+
+/* require("../src/routes/index")(app) */
+
+try {
+    app.listen(3001)
+    console.log("Servidor iniciado")
+} catch (error) {
+    console.log(error)
+}
+
--- src/view/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-const app = express()
-
-app.use()
