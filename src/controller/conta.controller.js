012ccf484cbbecdc0abc5416856e377d012da103
David Rocha <manoeudavi3@gmail.com>
2023-03-05 20:51:51 -0300

    configing the token

--- src/controller/conta.controller.js
+++ src/controller/conta.controller.js
@@ -13,3 +13,22 @@ export const create = async(req,res)=>{
         res.status(400).send(error)
     }
 }
+
+export const get = async (req,res) =>{
+    try {
+
+        const token = req.headers.authorization
+        const replace = token.replace("Bearer ", "")
+        const decoded = jwt.verify(replace, process.env.TOKEN_KEY)
+        const userId = decoded.id;
+        const userEmail = decoded.email;
+
+        const conta = await getAll()
+        const reponse = {conta, userId, userEmail}
+
+        res.status(200).send(reponse)
+        
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
