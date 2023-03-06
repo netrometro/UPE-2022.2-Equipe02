266e85ca3391e03841be1a2041a6f6334117e7d1
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-03-05 22:34:38 -0300

    feat: adding the get

--- src/controller/perfil.controller.js
+++ src/controller/perfil.controller.js
@@ -11,3 +11,12 @@ export const create = async(req,res)=>{
         res.status(400).send(error)
     }
 }
+
+export const get = async (req,res) =>{
+    try {
+        const perfil = await getAll()
+        res.status(200).send(perfil)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
