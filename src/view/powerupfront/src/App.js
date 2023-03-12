9daf5f242fb88547fef4eb51b96bdc6d89bf06dc
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-03-12 16:44:50 -0300

    feat: addind diet routes to acess

--- src/view/powerupfront/src/App.js
+++ src/view/powerupfront/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { SingUp } from "./Templates/SingUp/SingUp";
 import { Home } from "./Templates/Home/Home";
 import { Perfil } from "./Templates/Perfil/Perfil";
+import { Diet } from "./Templates/Diet/Diet";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route exact path='/cadastro' element={<SingUp/>}></Route>
           <Route exact path='/home' element={<Home/>}></Route>
           <Route exact path='/perfil' element={<Perfil/>}></Route>
+          <Route exact path='/diet' element={<Diet/>}></Route>
           </Routes>
         
         
---
+++ src/view/powerupfront/src/Templates/Diet/Diet.js
@@ -0,0 +1,16 @@
+import { Footers } from "../../Organism/Footer/Footer";
+import { Headers } from "../../Organism/Headers/Headers";
+
+
+
+
+export function Diet(){
+    return(
+        <div>
+            <Headers/>
+            <p>Criar dieta</p>
+            <p>Solicitar dieta</p>
+           <Footers/>
+        </div>
+    )
+}
