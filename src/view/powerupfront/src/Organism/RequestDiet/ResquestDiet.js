bdc61ccf68282c91dbfe3b74e0cd7121360b07ec
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-03-12 17:05:44 -0300

    add: adding the first diet

--- src/view/powerupfront/src/Organism/RequestDiet/ResquestDiet.js
+++ src/view/powerupfront/src/Organism/RequestDiet/ResquestDiet.js
@@ -11,6 +11,29 @@ export function RequestDiet(){
             "lancheT":[""],
             "janta":[""],
         },
+        "emagrecer":{
+            "cafe":["2 fatias de pão integral (60g) - ","2 ovos mexidos (120g) - "," 1 xícara de café com leite desnatado (240ml)"],
+            "lancheM":["1 maçã (150g) - ","1 punhado de castanhas (30g)"],
+            "almoco":["1 filé de frango grelhado (100g) - ","1 xícara de arroz integral cozido (120g) - ","1 xícara de feijão cozido (160g) - ","1 porção de legumes cozidos no vapor (200g) - ","1 taça de gelatina diet (120g)"],
+            "lancheT":["1 iogurte natural desnatado (200ml) - ","1 banana (100g)"],
+            "janta":["1 filé de salmão grelhado (100g) - ","1 xícara de quinoa cozida (120g) - ","1 porção de legumes cozidos no vapor (200g) - ","1 taça de salada de frutas (150g)"],
+        },
+        "ganhar":{
+            "cafe":["3 ovos inteiros mexidos (150g) - ","2 fatias de pão integral (60g) - ","1 banana (100g) - ","1 xícara de café com leite desnatado (240ml)"],
+            "lancheM":["1 porção de frutas (200g) - ","1 porção de nuts (30g)"],
+            "almoco":["120g de peito de frango grelhado - ","1 xícara de arroz integral cozido (120g) - ","1 xícara de feijão cozido (160g) - ","1 porção de legumes cozidos no vapor (200g) - ","1 porção de salada verde com azeite e limão"],
+            "lancheT":["1 iogurte natural desnatado (200ml) - ","1 scoop de whey protein (30g) - ","1 porção de frutas (100g)"],
+            "janta":["120g de filé de salmão grelhado - ","1 xícara de quinoa cozida (120g) - ","1 porção de legumes cozidos no vapor (200g) - ","1 porção de salada verde com azeite e limão"],
+
+        },
+        "manter":{
+            "cafe":["1 xícara de café com leite desnatado (240ml) - ","2 fatias de pão integral (60g) - ","1 colher de sopa de geleia sem açúcar (20g) - ","1 ovo cozido (50g) - ","1 laranja (150g)"],
+            "lancheM":["1 porção de frutas (200g) - ","1 iogurte natural desnatado (200ml)"],
+            "almoco":["1 filé de peixe grelhado (120g) - ","1 xícara de arroz integral cozido (120g) - ","1 xícara de feijão cozido (160g) - ","1 porção de legumes cozidos no vapor (200g) - ","1 porção de salada verde com azeite e limão"],
+            "lancheT":["1 porção de frutas (200g) - ","1 punhado de castanhas (30g)"],
+            "janta":["120g de peito de frango grelhado - ","1 batata doce cozida (150g) - ","1 porção de legumes cozidos no vapor (200g) - ","1 porção de salada verde com azeite e limão"],
+
+        },
     }
 
     return(
