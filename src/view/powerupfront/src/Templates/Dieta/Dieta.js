f678b8e8db7269183d24974df5c78a4ce9b58e1e
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-03-12 16:40:29 -0300

    add: creating diet component

---
+++ src/view/powerupfront/src/Templates/Dieta/Dieta.js
@@ -0,0 +1,15 @@
+
+import { Footers } from "../../Organism/Footers/Footers";
+import { Headers } from "../../Organism/Headers.js/Headers";
+
+
+export function Dieta(){
+    return(
+        <div>
+            <Headers/>
+            <p>Criar dieta</p>
+            <p>Solicitar dieta</p>
+            <Footers/>
+        </div>
+    )
+}
