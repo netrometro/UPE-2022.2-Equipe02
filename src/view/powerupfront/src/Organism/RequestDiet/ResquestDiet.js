563fb2e7b0452b3d071fadde4dd81a2863bdf205
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-03-12 16:54:08 -0300

    add: adding the contollers components to request

--- src/view/powerupfront/src/Organism/RequestDiet/ResquestDiet.js
+++ src/view/powerupfront/src/Organism/RequestDiet/ResquestDiet.js
@@ -1,7 +1,20 @@
+import { Buttons } from "../../Atomic/Buttons/Buttons";
+import { Inputs } from "../../Atomic/Input/Inputs";
+
 export function RequestDiet(){
     return(
         <div className="flex flex-col items-center justify-center pt-[20px]" >
             <p>Request Diet</p>
+            <div className=" w-max h-max p-[50px] shadow-2xl rounded-[12px] my-[30px] ">
+                    <select name="" id="" >
+                        <option value="emagrecimento">Emagrecimento</option>
+                        <option value="ganho">Ganho de massa</option>
+                        <option value="manter">Manter</option>
+                    </select>
+                    <p>Calorias na refeição:</p>
+                    <Inputs type="number"/>
+                    <Buttons type="submit" name="Enter"/>
+            </div>
         </div>
     )
 }
