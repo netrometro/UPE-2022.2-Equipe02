c1504ee56979d6e0f9376428f8d17e6259cb4458
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-03-12 17:04:29 -0300

    fix: fixing components names

--- src/view/powerupfront/src/Organism/RequestDiet/ResquestDiet.js
+++ src/view/powerupfront/src/Organism/RequestDiet/ResquestDiet.js
@@ -2,6 +2,17 @@ import { Buttons } from "../../Atomic/Buttons/Buttons";
 import { Inputs } from "../../Atomic/Input/Inputs";
 
 export function RequestDiet(){
+
+    const diet ={
+        "first":{
+            "cafe":[""],
+            "lancheM":[""],
+            "almoco":[""],
+            "lancheT":[""],
+            "janta":[""],
+        },
+    }
+
     return(
         <div className="flex flex-col items-center justify-center pt-[20px]" >
             <p>Request Diet</p>
@@ -9,40 +20,40 @@ export function RequestDiet(){
                     <select name="" id="" >
                         <option value="emagrecimento">Emagrecimento</option>
                         <option value="ganho">Ganho de massa</option>
-                        <option value="manter">Manter</option>
+                        <option value="manter">Keep</option>
                     </select>
                     
-                    <p>Calorias na refeição:</p>
+                    <p>Calories:</p>
                     <Inputs type="number"/>
                     <Buttons type="submit" name="Enter"/>
 
                     <div className=" w-[1300px] h-max p-[50px]  rounded-[12px] my-[30px] ">
                         <div className="">
-                            <p className="text-2xl font-bold text-[#8854d0]">Refeição</p>  
+                            <p className="text-2xl font-bold text-[#8854d0]">Food</p>  
                         </div>
 
                         <div className=" shadow-inner p-[20px] m-[10px] duration-500 hover:shadow-2xl ">
-                            <p className="my-[10px]">Café</p>
+                            <p className="my-[10px]">Break fast</p>
                             <p></p>
                         </div>
 
                         <div className=" shadow-inner p-[20px] m-[10px] duration-500 hover:shadow-2xl">
-                            <p className="my-[10px]">Lanche manhã</p>
+                            <p className="my-[10px]">Morning snacks</p>
                             <p></p>
                         </div>
 
                         <div className=" shadow-inner p-[20px] m-[10px] duration-500 hover:shadow-2xl">
-                            <p className="my-[10px]">Almoço</p>
+                            <p className="my-[10px]">Lunch</p>
                             <p></p>
                         </div>
 
                         <div className=" shadow-inner p-[20px] m-[10px] duration-500 hover:shadow-2xl">
-                            <p className="my-[10px]">Lanche tarde</p>
+                            <p className="my-[10px]">Afternoon snacks</p>
                             <p></p>
                         </div>
 
                         <div className=" shadow-inner p-[20px] m-[10px] duration-500 hover:shadow-2xl">
-                            <p className="my-[10px]">Jantar</p>
+                            <p className="my-[10px]">Dinner</p>
                             <p></p>
                         </div>
                     </div>
