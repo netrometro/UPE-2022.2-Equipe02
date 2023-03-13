e07afa893d37d579dc68ef7d40e9b4fc895b9d4a
David Rocha <manoeudavi3@gmail.com>
2023-03-12 21:49:22 -0300

    created the requeste to get the value from the database

--- src/view/powerupfront/src/Organism/getDiet/getDiet.js
+++ src/view/powerupfront/src/Organism/getDiet/getDiet.js
@@ -12,6 +12,58 @@ const{userId} = require("../CriarDieta/getId")
 
 export function GetDiet(){
 
+    axios.defaults.headers.common['Authorization'] = `Bearer `
+
+    const [ lanchesTarde, getLanchesTarde] = useState([])
+    const[almoco, getAlmoco] = useState([])
+    const[cafe, getCafe] = useState([])
+    const[janta, getJanta] = useState([])
+    const [ lanchesManha, getLanchesManha] = useState([])
+
+    useEffect(() => {
+        axios.get(`http://localhost:3001/lanche-da-tarde/${userId}`)
+        .then((response) => {
+            getLanchesTarde(response.data)
+        })
+        .catch(() =>{
+            console.log("deu tudo errado")
+        })
+
+
+        axios.get(`http://localhost:3001/almoco/${userId}`)
+        .then((response)=>{
+            getAlmoco(response.data)
+        })
+        .catch(()=>{
+            console.log("Há algum problema")
+        })
+
+        axios.get(`http://localhost:3001/cafe/${userId}`)
+        .then((response)=>{
+            getCafe(response.data)
+        })
+        .catch(()=>{
+            console.log("Há algum problema")
+        })
+
+        axios.get(`http://localhost:3001/jantar/${userId}`)
+        .then((response)=>{
+            getJanta(response.data)
+        })
+        .catch(()=>{
+            console.log("Há algum problema")
+        })
+
+        axios.get(`http://localhost:3001/lanche-da-manha/${userId}`)
+        .then((response)=>{
+            getLanchesManha(response.data)
+        })
+        .catch(()=>{
+            console.log("Há algum problema")
+        })
+
+    },[])
+
     return(
         <div>
              <Headers/>
