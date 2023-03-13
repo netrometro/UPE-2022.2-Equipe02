dd8c0eec83df822a64e5c3563667a79c82d0a1f5
David Rocha <manoeudavi3@gmail.com>
2023-03-12 22:36:43 -0300

    created the routes for lanche_manha

---
+++ src/routes/lanche_manha.routes.js
@@ -0,0 +1,13 @@
+import {create, remove, deleteAll, getByUser} from '../controllers/lanche_manha.controller'
+
+const rotas_lache_manha = app =>{
+    //inserir informação
+    app.post("/lanche-da-manha", create)
+    //listar
+    app.get("/lanche-da-manha/:id_conta", getByUser)
+    //deletar
+    app.delete("/lanche-da-manha/:id", remove)
+    app.delete("/lanche-da-manha-delete/:id_conta",deleteAll)
+}
+
+export default rotas_lache_manha
