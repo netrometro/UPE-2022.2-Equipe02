0e51d2da7e0a49a9fb36d02d97d87bdd7fe1fdb1
David Rocha <manoeudavi3@gmail.com>
2023-03-12 22:27:37 -0300

    created the methods to delete and update

--- src/repositories/lancheTarde.repository.js
+++ src/repositories/lancheTarde.repository.js
@@ -24,3 +24,44 @@ export const getFromUser = async(id_conta) =>{
     return lanche
 }
 
+//listar pelo id
+
+/* export const getById = async(id) =>{
+    const lanche = await prisma.lanche_tarde.findUnique({
+        where:{
+            id
+        }
+    })
+    return lanche
+} */
+
+//atualizar tabela
+export const updateLanche = async(id, data) => {
+    const lanche =  await prisma.lanche_tarde.update({
+        where:{
+            id
+        },
+        data
+    })
+    return lanche
+}
+
+//deletar da tabela
+
+export const deleteLanche = async(id) => {
+    await prisma.lanche_tarde.delete({
+        where:{
+            id
+        }
+    })
+    return
+}
+
+export const deleteTudo = async(id_conta) => {
+    await prisma.lanche_tarde.deleteMany({
+        where:{
+            id_conta
+        }
+    })
+    return deleteTudo
+}
