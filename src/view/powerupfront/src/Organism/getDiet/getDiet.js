71579ba3d67172108d3b6130801ba0ffc2eab289
David Rocha <manoeudavi3@gmail.com>
2023-03-12 21:39:50 -0300

    added the getDiet function

---
+++ src/view/powerupfront/src/Organism/getDiet/getDiet.js
@@ -0,0 +1,27 @@
+import { Footers } from "../Footers/Footers";
+import { Headers } from "../Headers.js/Headers";
+import { Link } from "react-router-dom";
+import { BackButtons } from "../../Atomic/Buttons/BackButtons";
+import { Buttons } from "../../Atomic/Buttons/Buttons";
+import axios, { isCancel, AxiosError } from "axios";
+import response from "react";
+import { useEffect, useState } from "react";
+import jsPDF from 'jspdf';
+const{userId} = require("../CriarDieta/getId")
+
+
+export function GetDiet(){
+
+    return(
+        <div>
+             <Headers/>
+            <div className="m-[20px]">
+                <Link to="/Dieta">
+                    <BackButtons name="<-"/>
+                </Link>
+            </div>
+        </div>
+       
+        
+    )
+}
