51d5fce567fd946bf8fcf7067ff9a1c16525dead
David Rocha <manoeudavi3@gmail.com>
2023-03-12 22:16:56 -0300

    created methods to delete adn get info  by id_conta attribute

--- src/controller/almoco.controller.js
+++ src/controller/almoco.controller.js
@@ -17,3 +17,49 @@ export const get = async (req,res) =>{
         res.status(400).send(error)
     }
 }
+
+/* export const getId = async(req,res) => {
+    try {
+        const almoco = await getById(Number(req.params.id))
+        res.status(200).send(almoco)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+} */
+
+export const getByUser = async(req,res) =>{
+    try {
+        const almoco = await getFromUser(Number(req.params.id_conta))
+        res.status(200).send(almoco)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+export const update = async(req, res) => {
+    try {
+        const almoco = await updateAlmoco(Number(req.params.id),req.body)
+        res.status(200).send(almoco)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+export const remove = async(req, res) => {
+    try {
+        await deleteAlmoco(Number(req.params.id))
+        res.status(200).send()
+    } catch (error) {
+        res.status(400).send(error)
+    }
+    
+}
+
+export const deleteAll = async(req, res) =>{
+    try{
+        await deleteTudo(Number(req.params.id_conta))
+        res.status(200).send("tudo deletado")
+    }catch(error){
+        console.log(error)
+    }
+}
