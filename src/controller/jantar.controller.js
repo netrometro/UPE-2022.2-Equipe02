1bdf867268308bae79ee80456d8842f5b3b112d5
David Rocha <manoeudavi3@gmail.com>
2023-03-12 22:22:04 -0300

    created the jantar.controller file along with functions from prisma

---
+++ src/controller/jantar.controller.js
@@ -0,0 +1,65 @@
+import{create_jantar, getAll, getById,updateJantar, deleteJantar, deleteTudo, getFromUser} from '../repositories/jantar.repository'
+
+export const create = async(req,res)=>{
+    try {
+        const jantar = await create_jantar(req.body)
+        res.status(200).send(jantar)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+export const get = async (req,res) =>{
+    try {
+        const jantar = await getAll()
+        res.status(200).send(jantar)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+/* export const getId = async(req,res) => {
+    try {
+        const jantar = await getById(Number(req.params.id))
+        res.status(200).send(jantar)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+} */
+
+export const getByUser = async(req,res) =>{
+    try {
+        const jantar = await getFromUser(Number(req.params.id_conta))
+        res.status(200).send(jantar)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+export const update = async(req, res) => {
+    try {
+        const jantar = await updateJantar(Number(req.params.id),req.body)
+        res.status(200).send(jantar)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+export const remove = async(req, res) => {
+    try {
+        await deleteJantar(Number(req.params.id))
+        res.status(200).send()
+    } catch (error) {
+        res.status(400).send(error)
+    }
+    
+}
+
+export const deleteAll = async(req, res) =>{
+    try{
+        await deleteTudo(Number(req.params.id_conta))
+        res.status(200).send("tudo deletado")
+    }catch(error){
+        console.log(error)
+    }
+}
--- src/repositories/jantar.repository.js
+++ src/repositories/jantar.repository.js
@@ -34,3 +34,34 @@ export const getFromUser = async(id_conta) =>{
     })
     return jantar
 }
+
+//atualizar tabela
+export const updateJantar = async(id, data) => {
+    const jantar =  await prisma.jantar.update({
+        where:{
+            id
+        },
+        data
+    })
+    return jantar
+}
+
+//deletar da tabela
+
+export const deleteJantar = async(id) => {
+    await prisma.jantar.delete({
+        where:{
+            id
+        }
+    })
+    return
+}
+
+export const deleteTudo = async(id_conta) => {
+    await prisma.jantar.deleteMany({
+        where:{
+            id_conta
+        }
+    })
+    return deleteTudo
+}
