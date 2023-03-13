a295723f6c6248cec1f91da6a72077edcc47d2b8
David Rocha <manoeudavi3@gmail.com>
2023-03-12 21:50:46 -0300

    created the function that will generate a pdf file of the meals

--- src/view/powerupfront/src/Organism/getDiet/getDiet.js
+++ src/view/powerupfront/src/Organism/getDiet/getDiet.js
@@ -64,6 +64,55 @@ export function GetDiet(){
 
     },[])
 
+    function gerarPdf() {
+
+        const doc = new jsPDF("portrait","mm",[597,410]);
+    
+
+        doc.setFont('bolditalic')
+        doc.setFontSize(15)
+        doc.setTextColor(136,84,207)
+        doc.setFont(undefined, 'bold')
+    
+        doc.text('Café', 20, 20);
+        var incrementador = 1;
+        cafe.forEach((item, index) => {
+            doc.text(`Aliemento ${incrementador}: ${item.nome_alimento}, Quantidade: ${item.quantidade} unidade(s), Proteínas: ${item.proteina}gm, Carboidratos: ${item.carboidrato}gm, Gorduras: ${item.gordura}gm.`,20, 40 + index * 10)
+            incrementador++
+        });
+
+        doc.text('Lanche da Manhã', 20, 100);
+        incrementador = 1
+        lanchesManha.forEach((item, index) => {
+            doc.text(`Aliemento ${incrementador}: ${item.nome_alimento}, Quantidade: ${item.quantidade} unidade(s), Proteínas: ${item.proteina}gm, Carboidratos: ${item.carboidrato}gm, Gorduras: ${item.gordura}gm.`, 20, 120 + index * 10)
+            incrementador++
+        });
+
+        doc.text('Almoço', 20, 180);
+        incrementador = 1
+        almoco.forEach((item, index) => {
+            doc.text(`Aliemento ${incrementador}: ${item.nome_alimento}, Quantidade: ${item.quantidade} unidade(s), Proteínas: ${item.proteina}gm, Carboidratos: ${item.carboidrato}gm, Gorduras: ${item.gordura}gm.`, 20, 200 + index * 10)
+            incrementador++
+        });
+
+        doc.text('Lanche da tarde', 20, 260);
+        incrementador = 1
+        lanchesTarde.forEach((item, index) => {
+            doc.text(`Aliemento ${incrementador}: ${item.nome_alimento}, Quantidade: ${item.quantidade} unidade(s), Proteínas: ${item.proteina}gm, Carboidratos: ${item.carboidrato}gm, Gorduras: ${item.gordura}gm.`, 20, 280 + index * 10)
+            incrementador++
+        });
+
+        doc.text('Jantar', 20, 340);
+        incrementador = 1
+        janta.forEach((item, index) => {
+            doc.text(`Aliemento ${incrementador}: ${item.nome_alimento}, Quantidade: ${item.quantidade} unidade(s), Proteínas: ${item.proteina}gm, Carboidratos: ${item.carboidrato}gm, Gorduras: ${item.gordura}gm.`, 20, 360 + index * 10)
+            incrementador++
+        });
+
+        doc.output("dataurlnewwindow")
+        }
+
+
     return(
         <div>
              <Headers/>
