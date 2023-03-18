f124ced814a7e4ac79ae6a09045e5dae04948c06
David Rocha <manoeudavi3@gmail.com>
2023-03-18 10:18:52 -0300

    starting the consts to get the values from inputs

--- src/view/powerupfront/src/Organism/createExercises/createExercises.js
+++ src/view/powerupfront/src/Organism/createExercises/createExercises.js
@@ -6,10 +6,65 @@ import axios, { isCancel, AxiosError } from "axios";
 import response from "react"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+const{userId} = require('../../getUser/getId')
 
 export function CreateExercise(){
 
+    const [nomeS, setNomeS] = useState("");
+    const [repeticaoS, setrepeticaoS] = useState("");
+    const [serieS, setSerieS] = useState("");
 
+    const [nomeT, setNomeT] = useState("");
+    const [repeticaoT, setrepeticaoT] = useState("");
+    const [serieT, setSerieT] = useState("");
+
+    const [nomeQ, setNomeQ] = useState("");
+    const [repeticaoQ, setrepeticaoQ] = useState("");
+    const [serieQ, setSerieQ] = useState("");
+
+    const [nomeQu, setNomeQu] = useState("");
+    const [repeticaoQu, setrepeticaoQu] = useState("");
+    const [serieQu, setSerieQu] = useState("");
+
+    const [nomeSx, setNomeSx] = useState("");
+    const [repeticaoSx, setrepeticaoSx] = useState("");
+    const [serieSx, setSerieSx] = useState("");
+
+    const [nomeSb, setNomeSb] = useState("");
+    const [repeticaoSb, setrepeticaoSb] = useState("");
+    const [serieSb, setSerieSb] = useState("");
+
+    const [nomeD, setNomeD] = useState("");
+    const [repeticaoD, setrepeticaoD] = useState("");
+    const [serieD, setSerieD] = useState("");
+
+    const handleNomeS = (event) => setNomeS(event.target.value);
+    const handlerepeticaoS = (event) => setrepeticaoS(event.target.value);
+    const handleserieS = (event) => setSerieS(event.target.value);
+
+    const handleNomeT = (event) => setNomeT(event.target.value);
+    const handlerepeticaoT = (event) => setrepeticaoT(event.target.value);
+    const handleserieT = (event) => setSerieT(event.target.value);
+
+    const handleNomeQ = (event) => setNomeQ(event.target.value);
+    const handlerepeticaoQ = (event) => setrepeticaoQ(event.target.value);
+    const handleserieQ = (event) => setSerieQ(event.target.value);
+
+    const handleNomeQu = (event) => setNomeQu(event.target.value);
+    const handlerepeticaoQu = (event) => setrepeticaoQu(event.target.value);
+    const handleserieQu = (event) => setSerieQu(event.target.value);
+
+    const handleNomeSx = (event) => setNomeSx(event.target.value);
+    const handlerepeticaoSx = (event) => setrepeticaoSx(event.target.value);
+    const handleserieSx = (event) => setSerieSx(event.target.value);
+
+    const handleNomeSb = (event) => setNomeSb(event.target.value);
+    const handlerepeticaoSb = (event) => setrepeticaoSb(event.target.value);
+    const handleserieSb = (event) => setSerieSb(event.target.value);
+
+    const handleNomeD = (event) => setNomeD(event.target.value);
+    const handlerepeticaoD = (event) => setrepeticaoD(event.target.value);
+    const handleserieD = (event) => setSerieD(event.target.value);
     return(
         <div className="flex flex-col items-center justify-center">
             <div className=" w-max h-max p-[50px] shadow-2xl rounded-[12px] my-[30px] ">
