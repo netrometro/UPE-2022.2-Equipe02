817ada6ed228a04dacd9398acd48f808348f8297
David Rocha <manoeudavi3@gmail.com>
2023-03-18 21:54:24 -0300

    added the tuesday exercise route

---
+++ src/routes/exer_terca.routes.js
@@ -0,0 +1,13 @@
+import {create, get,getId, update, remove, getByUser , deleteAll} from '../controllers/exer_terca.controller'
+
+const rotas_exer_terca = app =>{
+    //inserir informação
+    app.post("/terca", create)
+    //listar
+    app.get("/terca/:id_conta",getByUser)
+    //deletar
+    app.delete("/terca/:id", remove)
+    app.delete("/terca-delete/:id_conta", deleteAll)
+}
+
+export default rotas_exer_terca
