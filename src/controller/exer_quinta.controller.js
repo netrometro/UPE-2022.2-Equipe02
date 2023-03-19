601cb51d249178ba73e7e2c522d8d985c18dc7d7
David Rocha <manoeudavi3@gmail.com>
2023-03-18 21:44:19 -0300

    added the thursday exercise controller

---
+++ src/controller/exer_quinta.controller.js
@@ -0,0 +1,65 @@
+import { create_exer_quinta, getAll, getById,updateQuinta, deleteQuinta, getFromUser, deleteTudo } from "../repositories/exer_quinta.repository"
+
+export const create = async(req,res)=>{
+    try {
+        const quinta = await create_exer_quinta(req.body)
+        res.status(200).send(quinta)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+export const get = async (req,res) =>{
+    try {
+        const quinta = await getAll()
+        res.status(200).send(quinta)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+export const getId = async(req,res) => {
+    try {
+        const quinta = await getById(Number(req.params.id))
+        res.status(200).send(quinta)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+export const update = async(req, res) => {
+    try {
+        const quinta = await updateQuinta(Number(req.params.id),req.body)
+        res.status(200).send(quinta)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+export const remove = async(req, res) => {
+    try {
+        await deleteQuinta(Number(req.params.id))
+        res.status(200).send()
+    } catch (error) {
+        res.status(400).send(error)
+    }
+    
+}
+
+export const getByUser = async(req,res) =>{
+    try {
+        const quinta = await getFromUser(Number(req.params.id_conta))
+        res.status(200).send(quinta)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+export const deleteAll = async(req, res) =>{
+    try{
+        await deleteTudo(Number(req.params.id_conta))
+        res.status(200).send("tudo deletado")
+    }catch(error){
+        console.log(error)
+    }
+}
