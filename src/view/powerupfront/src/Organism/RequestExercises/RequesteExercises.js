006e3f5283d9645766995cc083f3841088d17b37
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-03-19 17:16:32 -0300

    add: adding the implementation of function to return

--- src/view/powerupfront/src/Organism/RequestExercises/RequesteExercises.js
+++ src/view/powerupfront/src/Organism/RequestExercises/RequesteExercises.js
@@ -109,6 +109,7 @@ export function RequestExercises(){
         }
     }
     
+    let typeFator = userFactor;
 
     return(
         <div>
@@ -138,37 +139,37 @@ export function RequestExercises(){
 
                         <div className=" shadow-inner p-[20px] m-[10px] duration-500 hover:shadow-2xl ">
                             <p className="my-[10px] font-bold">Dia 1</p>
-                            <p className="my-[10px] font-bold"></p>
+                            <p className="my-[10px] font-bold">{exercise[typeFator].parte1}</p>
                             <p></p>
                         </div>
 
                         <div className=" shadow-inner p-[20px] m-[10px] duration-500 hover:shadow-2xl ">
                             <p className="my-[10px] font-bold">Dia 2</p>
-                            <p className="my-[10px] font-bold"></p>
+                            <p className="my-[10px] font-bold">{exercise[typeFator].parte2}</p>
                             <p></p>
                         </div>
 
                         <div className=" shadow-inner p-[20px] m-[10px] duration-500 hover:shadow-2xl ">
                             <p className="my-[10px] font-bold">Dia 3</p>
-                            <p className="my-[10px] font-bold"></p>
+                            <p className="my-[10px] font-bold">{exercise[typeFator].parte3}</p>
                             <p></p>
                         </div>
 
                         <div className=" shadow-inner p-[20px] m-[10px] duration-500 hover:shadow-2xl ">
                             <p className="my-[10px] font-bold">Dia 4</p>
-                            <p className="my-[10px] font-bold"></p>
+                            <p className="my-[10px] font-bold">{exercise[typeFator].parte4}</p>
                             <p></p>
                         </div>
 
                         <div className=" shadow-inner p-[20px] m-[10px] duration-500 hover:shadow-2xl ">
                             <p className="my-[10px] font-bold">Dia 5</p>
-                            <p className="my-[10px] font-bold"></p>
+                            <p className="my-[10px] font-bold">{exercise[typeFator].parte5}</p>
                             <p></p>
                         </div>
 
                         <div className=" shadow-inner p-[20px] m-[10px] duration-500 hover:shadow-2xl ">
                             <p className="my-[10px] font-bold">Dia 6</p>
-                            <p className="my-[10px] font-bold"></p>
+                            <p className="my-[10px] font-bold">{exercise[typeFator].parte6}</p>
                             <p></p>
                         </div>
 
