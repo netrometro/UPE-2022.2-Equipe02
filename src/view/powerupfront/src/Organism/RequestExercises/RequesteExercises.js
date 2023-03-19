330f378e1c4cb33f4e7b43119db34309620e66c5
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-03-19 15:45:51 -0300

    add: creating section to show all commands

--- src/view/powerupfront/src/Organism/RequestExercises/RequesteExercises.js
+++ src/view/powerupfront/src/Organism/RequestExercises/RequesteExercises.js
@@ -1,7 +1,12 @@
 export function RequestExercises(){
     return(
         <div>
-            solicitar dieta
+            <div className="flex flex-col items-center justify-center pt-[20px]" >
+                <p className="text-2xl font-bold text-[#8854d0]">Solicitar Treino</p>
+                <div className=" w-max h-max p-[50px] shadow-2xl rounded-[12px] my-[30px] ">
+                    
+                </div>
+            </div>
         </div>
     )
 }
