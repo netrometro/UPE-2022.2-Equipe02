7c26d1cd4a85685482779674f51e1233bba4860f
David Rocha <manoeudavi3@gmail.com>
2023-03-18 21:56:30 -0300

    added the friday exercise route

---
+++ src/routes/exer_sexta.routes.js
@@ -0,0 +1,13 @@
+import {create,get,getId, update, remove, getByUser, deleteAll} from '../controllers/exer_sexta.controller'
+
+const rotas_exer_sexta = app =>{
+    //inserir informação
+    app.post("/sexta", create)
+    //listar
+    app.get("/sexta/:id_conta", getByUser)
+    //deletar
+    app.delete("/sexta/:id", remove)
+    app.delete("/sexta-delete/:id_conta", deleteAll)
+}
+
+export default rotas_exer_sexta
