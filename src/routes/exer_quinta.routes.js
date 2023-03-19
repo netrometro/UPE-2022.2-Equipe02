b8b91bd05fa54297d51dc75ba8f34e814191eb12
David Rocha <manoeudavi3@gmail.com>
2023-03-18 21:55:47 -0300

    added the thursday exercise route

---
+++ src/routes/exer_quinta.routes.js
@@ -0,0 +1,13 @@
+import {create,get,getId, update, remove, getByUser, deleteAll} from '../controllers/exer_quinta.controller'
+
+const rotas_exer_quinta = app =>{
+    //inserir informação
+    app.post("/quinta", create)
+    //listar
+    app.get("/quinta/:id_conta", getByUser)
+    //deletar
+    app.delete("/quinta/:id", remove)
+    app.delete("/quinta-delete/:id_conta", deleteAll)
+}
+
+export default rotas_exer_quinta
