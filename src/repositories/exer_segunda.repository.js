f0ea5a0ea7d623d7b7a1eeef561e4a1ce26c5db8
David Rocha <manoeudavi3@gmail.com>
2023-03-18 21:23:41 -0300

    added the monday exercise repository

---
+++ src/repositories/exer_segunda.repository.js
@@ -0,0 +1,71 @@
+import { prisma } from "../services/prisma";
+
+export const create_exer_segunda = async(data) =>{
+    const exer_segunda = await prisma.exerSegunda.create({
+        data
+    })
+    return exer_segunda
+}
+
+//listar todas as colunas
+
+export const getAll = async() =>{
+    const segunda = await prisma.exerSegunda.findMany({})
+    return segunda
+}
+
+//listar pelo id
+
+export const getById = async(id) =>{
+    const segunda = await prisma.exerSegunda.findUnique({
+        where:{
+            id
+        }
+    })
+    return segunda
+}
+
+
+export const getFromUser = async(id_conta) =>{
+    const segunda = await prisma.exerSegunda.findMany({
+        where:{
+            id_conta
+        }
+    })
+    return segunda
+}
+
+
+
+
+//atualizar tabela
+export const updateSegunda = async(id, data) => {
+    const segunda =  await prisma.exerSegunda.update({
+        where:{
+            id
+        },
+        data
+    })
+    return segunda
+}
+
+//deletar da tabela
+
+export const deleteSegunda = async(id) => {
+    await prisma.exerSegunda.delete({
+        where:{
+            id
+        }
+    })
+    return
+}
+
+//deletar tudo
+export const deleteTudo = async(id_conta) => {
+    await prisma.exerSegunda.deleteMany({
+        where:{
+            id_conta
+        }
+    })
+    return deleteTudo
+}
