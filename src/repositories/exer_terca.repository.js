bf8801c010e5aa9a6c49cc37bae3e798acf34164
David Rocha <manoeudavi3@gmail.com>
2023-03-18 21:25:38 -0300

    added the tuesday exercise repository

---
+++ src/repositories/exer_terca.repository.js
@@ -0,0 +1,71 @@
+import { prisma } from "../services/prisma";
+
+export const create_exer_terca = async(data) =>{
+    const exer_terca = await prisma.exerTerca.create({
+        data
+    })
+    return exer_terca
+}
+
+//listar todas as colunas
+
+export const getAll = async() =>{
+    const terca = await prisma.exerTerca.findMany({})
+    return terca
+}
+
+//listar pelo id
+
+export const getById = async(id) =>{
+    const terca = await prisma.exerTerca.findUnique({
+        where:{
+            id
+        }
+    })
+    return terca
+}
+
+
+export const getFromUser = async(id_conta) =>{
+    const terca = await prisma.exerTerca.findMany({
+        where:{
+            id_conta
+        }
+    })
+    return terca
+}
+
+
+
+
+//atualizar tabela
+export const updateTerca = async(id, data) => {
+    const terca =  await prisma.exerTerca.update({
+        where:{
+            id
+        },
+        data
+    })
+    return terca
+}
+
+//deletar da tabela
+
+export const deleteTerca = async(id) => {
+    await prisma.exerTerca.delete({
+        where:{
+            id
+        }
+    })
+    return
+}
+
+//deletar tudo
+export const deleteTudo = async(id_conta) => {
+    await prisma.exerTerca.deleteMany({
+        where:{
+            id_conta
+        }
+    })
+    return deleteTudo
+}
