6ea82e4b1d07f7dbe76a4f1744a8c0ff574b715d
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-03-26 16:52:56 -0300

    add: adding home componets title

--- front-end/src/Templates/Home/Home.js
+++ front-end/src/Templates/Home/Home.js
@@ -4,6 +4,7 @@ import { Headers } from "../../Organism/Headers/Headers.js";
 import { Footers } from "../../Organism/Footer/Footer.js";
 
 
+
 export function Home(){
 
 
@@ -12,10 +13,17 @@ export function Home(){
         
         <div>
             <Headers/>
-            <div>
-                <Link to='/'>
-                    <Buttons name="login"/>
-                </Link>
+            <div className="">
+                <div className="flex flex-col items-center">
+                <div className="flex flex-row">
+                        <h1 className="text-[50px] mt-[140px] font-bold text-[#8854d0]">Bem Vindo ao PowerUp</h1>
+                        
+                    </div>
+                    <p className="text-[20px] ">Uma aplicação feita para os amantes da musculação</p>
+                </div>
+                <div className="flex flex-col items-center">
+                    
+                </div>
             </div>
             <Footers/>
            
