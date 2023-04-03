4f6c71b43eb81edd3cd133ae8a9ccfa272cf861a
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-04-02 21:05:15 -0300

    feat: creating the select controller

--- front-end/src/Organism/Gym/FindGym.js
+++ front-end/src/Organism/Gym/FindGym.js
@@ -1,7 +1,16 @@
+import { Buttons } from "../../Atomic/Buttons/Buttons";
+
 export function FindGym(){
     return(
         <div>
-            FindGym
+            <div>Cidade disponíveis</div>
+            <select name="" id="">
+                <option value="">Selecione uma cidade</option>
+                <option value="1">Garanhuns</option>
+                <option value="2">Caruaru</option>
+                <option value="3">Recife</option>
+            </select>
+            <Buttons name="Pesquisar"/>
         </div>
     )
 }
