8bcc6bf22e1d5c67e06befd5c83f5d711f8ff786
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-04-02 21:10:20 -0300

    add: creating objext to save data

--- front-end/src/Organism/Gym/FindGym.js
+++ front-end/src/Organism/Gym/FindGym.js
@@ -1,15 +1,25 @@
 import { Buttons } from "../../Atomic/Buttons/Buttons";
 
 export function FindGym(){
+
+    const gym = {
+        "0":{
+           "name":[""],
+           "link":[""],
+        }
+    }
+
     return(
         <div>
             <div>Cidade disponíveis</div>
+
             <select name="" id="">
                 <option value="">Selecione uma cidade</option>
                 <option value="1">Garanhuns</option>
                 <option value="2">Caruaru</option>
                 <option value="3">Recife</option>
             </select>
+
             <Buttons name="Pesquisar"/>
         </div>
     )
