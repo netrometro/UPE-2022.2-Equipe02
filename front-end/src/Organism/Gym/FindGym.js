32040098360034422183f01a84ba5414d69bf302
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-04-02 21:17:32 -0300

    add: adding data to option 1

--- front-end/src/Organism/Gym/FindGym.js
+++ front-end/src/Organism/Gym/FindGym.js
@@ -8,8 +8,13 @@ export function FindGym(){
            "link":[""],
         },
         "1":{
-            "name":[""],
-            "link":[""],
+            "name":["Duo Academia","Happy Fit","Hit Academia", "FIIT Academia", "Selfit"],
+            "link":[    "https://www.google.com.br/maps/place/DUO+Academia+-+Unidade+Heli%C3%B3polis/@-8.8804528,-36.48695,17z/data=!3m1!4b1!4m6!3m5!1s0x7070cd95441d757:0x6e01c3530f0762d2!8m2!3d-8.8804581!4d-36.4847613!16s%2Fg%2F11cs6g060k",
+                        "https://www.google.com.br/maps/place/ACADEMIA+HAPPY+FIT+GARANHUNS/@-8.88787,-36.4918529,17z/data=!3m1!4b1!4m6!3m5!1s0x7070d4bdacb3917:0x6b06157047e39341!8m2!3d-8.8878753!4d-36.4896642!16s%2Fg%2F11rspf21gk",
+                        "https://www.google.com.br/maps/place/Hit+Academia/@-8.88787,-36.4918529,17z/data=!4m6!3m5!1s0x7070d20c05f5a33:0x1f6a88e03b2c5753!8m2!3d-8.882636!4d-36.477076!16s%2Fg%2F11f3c8z763",
+                        "https://www.google.com.br/maps/place/FIIT+Academia/@-8.8861459,-36.5024898,17z/data=!3m1!4b1!4m6!3m5!1s0x7070d7111fe795f:0x70dde98cfc79b4de!8m2!3d-8.8861512!4d-36.5003011!16s%2Fg%2F11ny0xvs15",
+                        "https://www.google.com.br/maps/place/Selfit/@-8.886221,-36.4888906,17z/data=!3m1!4b1!4m6!3m5!1s0x7070d4679a13e51:0xbadcdf753fdf53b1!8m2!3d-8.8862263!4d-36.4867019!16s%2Fg%2F11tbwnx51z"
+        ],
          },
          "2":{
             "name":[""],
