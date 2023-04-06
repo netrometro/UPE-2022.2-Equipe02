7d676b29b499ad1d880f2c685fabc5eaefecafee
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-04-06 17:01:22 -0300

    style: adding more function to footer

--- front-end/src/Organism/Footer/Footer.js
+++ front-end/src/Organism/Footer/Footer.js
@@ -1,16 +1,64 @@
+import { IconBrandInstagram, IconBrandLinkedin, IconBrandGithub, IconCode } from '@tabler/icons-react';
+import { Link } from "react-router-dom";
+
 export function Footers(){
     return(
-        <div className="col-span-12 bg-[#341f97] h-max text-[#fff]">
-                <div className="flex flex-row">
-                    <div className="m-[50px]">
-                        <p>David</p>
-                    </div>
-                    <div className="m-[50px]">
-                        <p>
-                            Jamuelton
+        <div className="bg-[#341f97] text-[#fff]">
+        
+                
+                <div className="flex flex-row justify-center">
+                <div className='m-[50px] shadow-inner p-[20px] bg-[#27157a] rounded-[12px]'>
+                        <p >
+                            <Link to="https://github.com/DavidRochaCode/PowerUp" className='flex flex-row w-[120px] justify-around '>
+                                <IconCode/>
+                                <p>Repositório</p>
+                            </Link>
                         </p>
-                    </div>
                 </div>
+                    <div className="flex flex-row m-[50px] shadow-inner p-[20px] bg-[#27157a] rounded-[12px] duration-500 hover:bg-[#fff] hover:text-[#27157a]">
+                        <p className='font-bold'>David</p>
+                        <ul className="flex flex-row w-[100px] justify-around">
+                            <li className='cursor-pointer duration-500 hover:p-[5px]'>
+                                <Link to="https://www.instagram.com/davidemmannoel/">
+                                    <IconBrandInstagram/>
+                                </Link>
+                                </li>
+                            <li className='cursor-pointer duration-500 hover:p-[5px]'>
+                                <Link to="https://www.linkedin.com/in/david-emmanoel-332132179/">
+                                    <IconBrandLinkedin/>
+                                </Link>
+                                </li>
+                            <li className='cursor-pointer duration-500 hover:p-[5px]'>
+                                <Link to="https://github.com/DavidRochaCode">
+                                    <IconBrandGithub/>
+                                </Link>
+                                </li>
+                        </ul>
+                    </div>
+
+                    <div className="flex flex-row m-[50px] shadow-inner p-[20px] bg-[#27157a] rounded-[12px] duration-500 hover:bg-[#fff] hover:text-[#27157a]">
+                        <p className='font-bold'>Jamuelton</p>
+                        <ul className="flex flex-row w-[100px] justify-around">
+                            <li className='cursor-pointer duration-500 hover:p-[5px]' >
+                                <Link to="https://www.instagram.com/jamuelton/">
+                                    <IconBrandInstagram/>
+                                </Link>
+                            </li>
+                            <li className='cursor-pointer duration-500 hover:p-[5px]'>
+                                <Link to="https://www.linkedin.com/in/jamuelton-angelim-0b26b41bb/">
+                                    <IconBrandLinkedin/>
+                                </Link>
+                            </li>
+                            <li className='cursor-pointer duration-500 hover:p-[5px]'>
+                                <Link to="https://github.com/Jamuelton">
+                                    <IconBrandGithub/>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <hr className='text-[#fff]'></hr>
+                </div>
+            
         </div>
     )
 }
--- front-end/src/Templates/SingUp/SingUp.js
+++ front-end/src/Templates/SingUp/SingUp.js
@@ -1,12 +1,13 @@
 import { HeadersLogin } from "../../Organism/Headers/HeadersLogin";
 import { SingAreas } from "../../Organism/SingArea/SingAreas.js";
+import { IconArrowLeft } from '@tabler/icons-react';
 
 export function SingUp(){
     return(
         <div className="grid grid-cols-12">
             <HeadersLogin>
             </HeadersLogin>
-            <SingAreas title="PowerUp" subTitle="Cadastro" but1="Cadastrar" but2="voltar"/>
+            <SingAreas title="PowerUp" subTitle="Cadastro" but1="Cadastrar" but2={<IconArrowLeft/>}/>
         </div>
     )
 }
